0dc7bc6494a7554e40469f1c527a0fa803ea0947
Joey6-z <jzen0041@student.monash.edu>
2026-05-15 20:23:44 +1000

    S1: edit chart3

--- 2179_A2/main.js
+++ 2179_A2/main.js
@@ -1,7 +1,8 @@
 var vg1 = "js/S1Scatter.vg.json";
-
-vegaEmbed("#chart1", vg1).then(function(result) {
-}).catch(console.error);
+vegaEmbed("#chart1", vg1).catch(console.error);
 
 var vg2 = "js/S1Heatmap.vg.json";
-vegaEmbed("#chart2", vg2);
+vegaEmbed("#chart2", vg2).catch(console.error);
+
+var vg3 = "js/S1Bar.vg.json";
+vegaEmbed("#chart3", vg3).catch(console.error);
